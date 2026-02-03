b3cc5938ba1db25ca6c3845536c2181551390060
lonewolfyx <877085893@qq.com>
2026-02-03 23:56:22 +0800

    feat(cli): migrate CLI from cac to citty with skill discovery

--- src/cli.ts
+++ src/cli.ts
@@ -1,13 +1,47 @@
-import cac from 'cac'
-import { name, version } from '../package.json'
+import type { IAgentType, IConfig } from '@/types.ts'
+import { join } from 'node:path'
+import { defineCommand, runMain } from 'citty'
+import { glob } from 'glob'
+import { resolveConfig } from '@/config.ts'
+import { description, name, version } from '../package.json'
 
-const cli = cac(name)
+const getSkillPath = (config: IConfig, agentType: IAgentType): string => {
+    const agent = config.agents[agentType]
 
-cli.command('')
-    .option('--test -t', 'arg description', { default: false })
-    .action(() => {
-    })
+    return config.global
+        ? agent.globalSkillsDir
+        : join(config.cwd, agent.skillsDir)
+}
 
-cli.help()
-cli.version(version)
-cli.parse()
+const main = defineCommand({
+    meta: {
+        name,
+        version,
+        description,
+    },
+    args: {
+        global: {
+            type: 'boolean',
+            description: 'Global option',
+            alias: 'g',
+            default: false,
+        },
+    },
+    setup() {
+        console.log('welcome use clear skill cli tool')
+    },
+    async run({ args }) {
+        const config = resolveConfig(args)
+
+        for (const agent of Object.keys(config.agents) as IAgentType[]) {
+            const agentDir = getSkillPath(config, agent)
+            // console.log(agent, agentDir);
+            const skills = await glob(`${agentDir}/**/SKILL.md`)
+            if (skills.length) {
+                console.log(skills)
+            }
+        }
+    },
+})
+
+await runMain(main)
